8f762489a01d68bda199f2d87b4cc953a675d62d
Sarah <sninwood@gmail.com>
2018-10-02 15:17:20 +1300

    merging uniprot venom annots with blastx contig res

---
+++ src/merging_uniprot_venom_blastx.R
@@ -0,0 +1,17 @@
+library(data.table)
+library(Biostrings)
+
+##read in contig annotations from uniprot venom db and rename columns
+uniprot_venom_results <- fread("output/uniprot_venom_contigs/blastx.outfmt6")
+setnames(uniprot_venom_results, old=c("V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "V10", "V11", "V12"), new=c("contig_id", "uniprot_venom_id", "%_identical_matches", "alignment_length", "no_mismatches", "no_gap_openings", "query_start", "query_end", "subject_start", "subject_end", "evalue", "bit_score"))
+
+##Read in uniprotdb, keep only rownames and format as needed for merge
+uniprot_db <- data.frame(readDNAStringSet("data/uniprot_toxin_venom_db.fasta"))
+uniprot_db <- setDT(uniprot_db, keep.rownames = TRUE)
+uniprot_db_names <- data.table(uniprot_db$rn)
+uniprot_db_names_split <- uniprot_db_names[,tstrsplit(V1, "|", fixed = TRUE)]
+uniprot_db_names_split$V1 <- NULL
+setnames(uniprot_db_names_split, old=c("V2", "V3"), new=c("uniprot_venom_id", "uniprot_annotation"))
+
+contig_uniprot_blastx <- merge(uniprot_venom_results, uniprot_db_names_split, by.x= "uniprot_venom_id", by.y = "uniprot_venom_id")
+fwrite(contig_uniprot_blastx, "output/uniprot_venom_contigs/contig_uniprot_blastx_annotations.csv")
